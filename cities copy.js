90253a3171ac3fd0b4db4be927a900c012fc3f28
Gunduz E <66138491+Ageslense@users.noreply.github.com>
2022-04-24 19:33:42 +0300

    Update cities copy.js

--- cities copy.js
+++ cities copy.js
@@ -76,11 +76,11 @@ async function populator(data){
 
             let totalPrice = 0;
 
-            data.calendar.days.forEach( e => {
+            data.days.forEach( e => {
                 price = totalPrice + e.day_rate
             })
 
-            const price = Math.round(totalPrice / data.calendar.days.length)
+            const price = Math.round(totalPrice / data.days.length)
 
             e.parentElement.querySelector('city-unit-price').textContent = price
         }
